f9a6680b4b2ab2b16dff4956c8cb88a65bb8f09a
David Cornejo <drcornejo19@gmail.com>
2026-05-21 12:30:59 -0300

    Integrate about page with AppShell

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,14 @@
+import { AppShell } from "@/components/AppShell";
 import { HowRefLabWasBorn } from "@/components/HowRefLabWasBorn";
 
 export default function AboutPage() {
   return (
-    <main className="min-h-screen bg-[#020b14] px-6 py-20">
-      <div className="mx-auto max-w-7xl">
-        <HowRefLabWasBorn />
-      </div>
-    </main>
+    <AppShell>
+      <main className="min-h-screen bg-[#020b14] px-6 py-20">
+        <div className="mx-auto max-w-7xl">
+          <HowRefLabWasBorn />
+        </div>
+      </main>
+    </AppShell>
   );
 }
